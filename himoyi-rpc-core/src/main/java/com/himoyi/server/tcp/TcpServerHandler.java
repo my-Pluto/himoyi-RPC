274d149f40703a4a7d24834a49d1f89d1ce9b59e
zhangyufei <zhangyufei@stu.qut.edu.cn>
2024-12-13 16:35:24 +0800

    完成请求处理器部分

--- himoyi-rpc-core/src/main/java/com/himoyi/server/tcp/TcpServerHandler.java
+++ himoyi-rpc-core/src/main/java/com/himoyi/server/tcp/TcpServerHandler.java
@@ -1,4 +1,93 @@
 package com.himoyi.server.tcp;
 
-public class TcpServerHandler {
+import com.himoyi.model.RpcRequest;
+import com.himoyi.model.RpcResponse;
+import com.himoyi.protocol.ProtocolMessage;
+import com.himoyi.protocol.ProtocolMessageDecoder;
+import com.himoyi.protocol.ProtocolMessageEncoder;
+import com.himoyi.protocol.ProtocolMessageTypeEnum;
+import com.himoyi.registry.LocalRegistry;
+import io.vertx.core.Handler;
+import io.vertx.core.buffer.Buffer;
+import io.vertx.core.net.NetSocket;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.lang.reflect.Method;
+
+/**
+ * TCP请求处理器，用于实现映射到具体的服务类上执行方法并返回结果
+ */
+@Slf4j
+public class TcpServerHandler implements Handler<NetSocket> {
+    @Override
+    public void handle(NetSocket netSocket) {
+
+        // 收到请求后进行处理
+        netSocket.handler(buffer -> {
+            ProtocolMessage<RpcRequest> protocolMessage;
+
+            try {
+                // 获取请求信息，并解码
+                protocolMessage = (ProtocolMessage<RpcRequest>) ProtocolMessageDecoder.decode(buffer);
+            } catch (IOException e) {
+                log.error("消息解码错误！");
+                throw new RuntimeException("消息解码错误！");
+            }
+
+            // 执行请求的方法
+            RpcResponse response = invokeMethod(protocolMessage.getData());
+
+            // 构造返回消息
+            ProtocolMessage<RpcResponse> responseMessage = createResponse(protocolMessage, response);
+
+            try {
+                // 对返回消息进行编码
+                Buffer encode_ProtocolResponseMessage = ProtocolMessageEncoder.encode(responseMessage);
+                // 写入返回消息
+                netSocket.write(encode_ProtocolResponseMessage);
+            } catch (IOException e) {
+                log.error("编码错误！");
+                throw new RuntimeException("编码错误", e);
+            }
+        });
+    }
+
+    /**
+     * 执行远程调用
+     *
+     * @param request 请求消息
+     * @return 响应结果
+     */
+    private RpcResponse invokeMethod(RpcRequest request) {
+        try {
+            Class<?> service = LocalRegistry.get(request.getServiceName());
+            Method method = service.getMethod(request.getMethodName(), request.getParameterTypes());
+            Object result = method.invoke(service.getDeclaredConstructor().newInstance(), request.getParameters());
+
+            RpcResponse response = new RpcResponse();
+            response.setData(result);
+            response.setDataType(method.getReturnType());
+            response.setMessage("SUCCESS");
+            return response;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * 构造返回信息
+     *
+     * @param protocolMessage 协议消息
+     * @param response        执行结果
+     * @return 协议消息
+     */
+    private ProtocolMessage<RpcResponse> createResponse(ProtocolMessage<RpcRequest> protocolMessage, RpcResponse response) {
+        // 构造返回消息
+        ProtocolMessage.Header header = protocolMessage.getHeader();
+        header.setType((byte) ProtocolMessageTypeEnum.RESPONSE.getKey());
+        return new ProtocolMessage<>(header, response);
+
+    }
+
 }
