64c022be158bb00d62e960222ebee7510ce0cb62
zhangyufei <zhangyufei@stu.qut.edu.cn>
2024-12-16 21:32:51 +0800

    创建注解驱动开发模块

---
+++ himoyi-rpc-spring-boot-starter/src/main/java/com/himoyi/himoyirpcspringbootstarter/HimoyiRpcSpringBootStarterApplication.java
@@ -0,0 +1,13 @@
+package com.himoyi.himoyirpcspringbootstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HimoyiRpcSpringBootStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HimoyiRpcSpringBootStarterApplication.class, args);
+    }
+
+}
---
+++ himoyi-rpc-spring-boot-starter/src/test/java/com/himoyi/himoyirpcspringbootstarter/HimoyiRpcSpringBootStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.himoyi.himoyirpcspringbootstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HimoyiRpcSpringBootStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
